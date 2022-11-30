9696cd20508dca4f4497de560c96c272a9f58d9a
unknown <klinkamama@gmail.com>
2022-11-30 19:14:08 +0300

    near

--- prac8/Program.cs
+++ prac8/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            mainfile.lol();
+            mainfile.writetext();
         }
     }
 }
--- prac8/a.cs
+++ prac8/a.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace prac8
 {
-    internal class a
+    internal static class a
     {
+        private static Random rand = new Random();
+        private static int number = rand.Next(0, 11);
+        public static string currentstroka = File.ReadLines("C:\\Users\\klink\\OneDrive\\Рабочий стол\\texts.txt").ElementAt(number);
     }
 }
